bd84c15f0b552d0f34124212fc4c64d36db5ce51
Herik dos Santos Lorenção <heriksantoslorencao@gmail.com>
2025-03-30 18:21:31 -0300

    feat(frontend): adiciona estrutura inicial do header
    Adiciona header e configurações do tailwind e chad cnd

---
+++ frontend/src/lib/components/shared/header/header.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+
+</script>
+
+<header class="flex border-b-2 border-gray-200 dark:border-gray-800">
+	<nav class="h-10 flex items-center">
+		<a class="flex items-center px-5 hover:bg-gray-800 h-full" href="/">Inicio</a>
+	</nav>
+</header>
---
+++ frontend/src/routes/+layout.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+	import '../app.css';
+	import { Header } from '$lib/components/shared/header';
+	let { children } = $props();
+</script>
+
+<Header />
+{@render children()}
